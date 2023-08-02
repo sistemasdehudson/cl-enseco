9cffdfd6c66b9130faf11112b3ce14366cf3cf99
concagabriel <58643247+concagabriel@users.noreply.github.com>
2023-08-02 01:20:14 -0300

    Update __manifest__.py

--- enseco_default/__manifest__.py
+++ enseco_default/__manifest__.py
@@ -19,14 +19,14 @@
 
     # Config to write in odoo.conf
     'config': [
+            'workers = 6',
+            'server_wide_modules = web,queue_job',
     #'limit_request': '8196',
     #'limit_memory_soft': '640000000',
     #'limit_memory_hard': '760000000',
     #'limit_time_cpu': '60',
     #'limit_time_real': '120',
     #'dbfilter': 'goldway',
-        'workers = 6',
-        'server_wide_modules = web,queue_job',
     ],
     'port': '8069',
     'git-repos': [
