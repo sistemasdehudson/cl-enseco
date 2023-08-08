4d86503695c6a3d692470bd6add1bd0aae66fe1d
concagabriel <58643247+concagabriel@users.noreply.github.com>
2023-08-08 02:46:00 -0300

    Update __manifest__.py

--- enseco_default/__manifest__.py
+++ enseco_default/__manifest__.py
@@ -87,6 +87,7 @@
         'https://github.com/OCA/bank-statement-import -b 13.0',
          ##modulos stock para remito
         'https://github.com/OCA/stock-logistics-workflow.git -b 13.0',
+        'https://github.com/OCA/brand.git -b 13.0',
     ],
     # list of images to use in the form 'name image-url'
     'docker-images': [
